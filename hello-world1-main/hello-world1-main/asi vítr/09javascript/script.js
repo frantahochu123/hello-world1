ef856b51a4c02d45469867f21a4a62f9890a2451
frantahochu123 <145441198+frantahochu123@users.noreply.github.com>
2023-11-07 13:11:34 +0100

    ok

--- hello-world1-main/hello-world1-main/asi vítr/09javascript/script.js
+++ hello-world1-main/hello-world1-main/asi vítr/09javascript/script.js
@@ -5,14 +5,25 @@ function checkResult() {
         } else if (inputNumber > 18) {
             console.log("dospělý uživatel")
         }
-        
         if (inputNumber > 2004) {
             console.log("dětský rok")
-        } else if (inputNumber < 2005) {
-            console.log("dospělý rok")
         }
+        else if (inputNumber < 2005) {
+            console.log("dospělý rok")
+        } 
+}
+
+function forCycle(){
+    let userInput = parseInt(document.getElementById("cycleInput").value)
+
+    let output =""
+
+    for(let i = 0; i <= userInput; i++) {
+        output = output + "."
+    }
 
 
+    document.getElementById("text").innerHTML = output
 }
 
 
